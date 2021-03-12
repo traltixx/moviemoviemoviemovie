50109ef6147b840201838fb6d8a2b8084cbaac08
traltixx <k12umm@hotmail.com>
2021-03-13 09:16:50 +1100

    Add files via upload
    Updated to prepare it for web version

--- make_movie.py
+++ make_movie.py
@@ -39,55 +39,64 @@ if os.path.exists(r'movieresult.txt'):
 	with open(r'movieresult.txt','r') as infile:
 		for line in infile:
 			lines.append(line)
-	done = False
-	while not done:
-		movie = random.choice(lines)
-		movie1 = movie.split(' +++ ')[0].split(') ')[1].strip()
-		movie2 = movie.split(' +++ ')[1].split(') ')[1].split(' ==>')[0].strip()
-		movie1title = movie.split(' +++ ')[0].split(' (')[0].strip()
-		movie2title = movie.split(' +++ ')[1].split(' (')[0].strip()
-		movieanswer = movie.split(' ==>')[1].strip()
-		movie1plot = ''
-		movie2plot = ''
-		with open(r"MovieSummaries\plot_summaries.txt",'r') as infile:
-			for line in infile:
-				if line.startswith(movie1):
-					movie1plot = line.split(movie1)[1].strip()
-				if line.startswith(movie2):
-					movie2plot = line.split(movie2)[1].strip()
-		if movie1plot != '' and movie2plot != '' and len(movie1plot.split(' ')) > 50 and len(movie2plot.split(' ')) > 50:
-			done = True
+	fpo = open(r'results.txt','w')
+	for work in lines:
+	#done = False
+	#while not done:
+		#movie = random.choice(lines)
+		movie = work
+		done = False
+		if movie.find(' +++ ') > -1 and movie.find(' ==>') > -1:
+			print movie
+			movie1 = movie.split(' +++ ')[0].split(') ')[1].strip()
+			movie2 = movie.split(' +++ ')[1].split(') ')[1].split(' ==>')[0].strip()
+			movie1title = movie.split(' +++ ')[0].split(' (')[0].strip()
+			movie2title = movie.split(' +++ ')[1].split(' (')[0].strip()
+			movieanswer = movie.split(' ==>')[1].strip()
+			movie1plot = ''
+			movie2plot = ''
+			
+			with open(r"MovieSummaries\plot_summaries.txt",'r') as infile:
+				for line in infile:
+					if line.startswith(movie1):
+						movie1plot = line.split(movie1)[1].strip()
+					if line.startswith(movie2):
+						movie2plot = line.split(movie2)[1].strip()
+			if movie1plot != '' and movie2plot != '' and len(movie1plot.split(' ')) > 50 and len(movie2plot.split(' ')) > 50:
+				done = True
 	#same word
+		if done:
+			movie1words = movie1plot.split(' ')[30:50]
+			movie2words = movie2plot.split(' ')[-50:-30]
 
+			pos1 = -1
+			position1=0
+			pos2 = -1
+			
+			for movie1word in movie1words:	
+				position2 =0	
+				for movie2word in movie2words:
+					if movie1word.lower() == movie2word.lower():				
+						pos1 = position1
+						pos2 = position2
 
+					position2+=1
+				position1+=1
+			pos2 = 20-pos2
 
-	movie1words = movie1plot.split(' ')[30:50]
-	movie2words = movie2plot.split(' ')[-50:-30]
+			if pos1 > -1 and pos2 > -1:
 
-	pos1 = 0
-	position1=0
-	pos2 = 0
-	
-	for movie1word in movie1words:	
-		position2 =0	
-		for movie2word in movie2words:
-			if movie1word.lower() == movie2word.lower():				
-				pos1 = position1
-				pos2 = position2
-
-			position2+=1
-		position1+=1
-	pos2 = 20-pos2
-
-	result = ' '.join(movie1plot.split(' ')[0:30+pos1]) + ' '.join(movie2plot.split(' ')[-30-pos2:])
-	
-	if result.lower().find(movie1title.lower()) > -1:
-		result = re.sub(movie1title,' <REDACTED> ',result,re.IGNORECASE)
-	if result.lower().find(movie2title.lower()) > -1:
-		result = re.sub(movie2title,' <REDACTED> ',result,re.IGNORECASE)
-
-	print 'Question :' , result
-	print '======================='
-	print 'Answer is : ', movieanswer
+				result = ' '.join(movie1plot.split(' ')[0:30+pos1]) + ' '.join(movie2plot.split(' ')[-30-pos2:])
+				
+				if result.lower().find(movie1title.lower()) > -1:
+					result = re.sub(movie1title,' <REDACTED> ',result,re.IGNORECASE)
+				if result.lower().find(movie2title.lower()) > -1:
+					result = re.sub(movie2title,' <REDACTED> ',result,re.IGNORECASE)
+				fpo.write("-----------------------\n")
+				fpo.write('Question :' + result + '\n')
+				fpo.write('=======================\n')
+				fpo.write('Answer is : '+ movieanswer+'\n')
+				fpo.write("***********************\n")
+	fpo.close()	
 
 print("--- %s seconds ---" % (time.time() - start_time))
